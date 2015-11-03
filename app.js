b0d04631ec3ec7040a560c740237f27ea70f4ec6
99dice <rigged@protonmail.com>
2015-11-03 18:21:47 +0100

    test

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ helpers.multiplierToWinProb = function(multiplier) {
   console.assert(multiplier > 0);
 
   // For example, n is 0.99 when house edge is 1%
-  var n = 1.0 - config.house_edge;
+  var n = 2.97 - config.house_edge;
 
   return n / multiplier;
 };
